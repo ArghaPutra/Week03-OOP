60c64072505002a7196c509166a1d7ad26166716
ArghaPutra <alifarghaptra@gmail.com>
2025-09-10 11:28:28 +0700

    tes

---
+++ src/PreludeImmutability.java
@@ -0,0 +1,2 @@
+public class PreludeImmutability {
+}
---
+++ src/PreludeString.java
@@ -0,0 +1,14 @@
+public class PreludeString {
+    public static void main(String[] args);
+    String pbo1 = "Pemrograman Berorientasi Objek";
+    String pbo2 = new String("Pemrograman Berorientasi Objek");
+
+    pbo1.length();
+    pbo1.charAt(12);
+    pbo1.substring(24);
+    pbo1.substring(12, 24);
+
+    string kode = "IF402 - ";
+    String out = kode.concat(pbo1);
+    }
+}
